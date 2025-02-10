be2bb3fd58c5975efae18d2a289362599cd81d21
HiroshySystems <hiroshysystems@gmail.com>
2025-02-09 19:45:05 -0500

    update-entity-ventas-ventas-det

--- src/main/java/com/practice/entity/Venta.java
+++ src/main/java/com/practice/entity/Venta.java
@@ -1,6 +1,7 @@
 package com.practice.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import java.util.Date;
 import lombok.*;
@@ -15,25 +16,31 @@ import java.util.List;
 public class Venta {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("saleId")
     private Integer idVenta;
     
     @Column(nullable = false)
+    @JsonProperty("customerId")
     private Integer idCliente;
     
     @Column(nullable = false)
+    @JsonProperty("saleDate")
     private Date feVenta;
     
     @Column(nullable = false)
+    @JsonProperty("totalAmount")
     private Double imTotal;
     
     @Column(nullable = false)
+    @JsonProperty("itemCount")
     private Integer nuItems;
     
     @Column(nullable = false)
+    @JsonProperty("userId")
     private Integer idUsuario;
     
     @OneToMany(mappedBy = "venta") /*, cascade = CascadeType.ALL, fetch = FetchType.LAZY*/
     @JsonManagedReference
+    @JsonProperty("saleDetails")
     private List<VentaDetalle> detalles;
 }
-
--- src/main/java/com/practice/entity/VentaDetalle.java
+++ src/main/java/com/practice/entity/VentaDetalle.java
@@ -1,6 +1,7 @@
 package com.practice.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,22 +14,28 @@ import lombok.*;
 public class VentaDetalle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("saleDetailId")
     private Integer idVentaDet;
     
     @ManyToOne
     @JoinColumn(name = "id_venta", nullable = false)
     @JsonBackReference
+    @JsonProperty("sale")
     private Venta venta;
     
     @Column(nullable = false)
+    @JsonProperty("videoGameId")
     private Integer idVideoJuego;
     
     @Column(nullable = false)
+    @JsonProperty("quantity")
     private Integer nuCantidad;
     
     @Column(nullable = false)
+    @JsonProperty("salePrice")
     private Double imPrecioVenta;
     
     @Column(nullable = false)
+    @JsonProperty("totalAmount")
     private Double imTotal;
 }
